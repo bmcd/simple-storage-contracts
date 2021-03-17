e1207210f029c092aefa2a45f470ee84c61fee52
Brad McDermott <bradmcdermott@gmail.com>
2021-03-17 14:34:23 -0400

    Deploys to ropsten, adds .env support for secrets, fixes hardhat network id, adds etherscan verification

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,7 @@
+require("@nomiclabs/hardhat-etherscan");
 require("@nomiclabs/hardhat-waffle");
 require('@openzeppelin/hardhat-upgrades');
+require('dotenv-flow').config()
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -21,11 +23,18 @@ module.exports = {
   solidity: "0.6.12",
   networks: {
     hardhat: {
+      chainId: 1337,
     },
     quorum: {
       url: "http://127.0.0.1:22000",
-
-    }
+    },
+    // ropsten: {
+    //   url: process.env.ROPSTEN_INFURA_URL,
+    //   accounts: [process.env.ROPSTEN_PRIVATE_KEY],
+    // }
   },
+  // etherscan: {
+  //   apiKey: process.env.ETHERSCAN_API_KEY,
+  // },
 };
 
